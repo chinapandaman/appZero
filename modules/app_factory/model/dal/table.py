382e2aac7a1831be7035e2f515bdc9dc7cca67ed
chinapandaman <b.j.li@foxmail.com>
2019-11-03 17:49:22 -0600

    more exception

--- modules/app_factory/model/dal/table.py
+++ modules/app_factory/model/dal/table.py
@@ -31,7 +31,7 @@ class Table(AppZero):
             for table in obj:
                 if table["table_name"] in existed_table:
                     raise Exception(
-                        "duplicate table: {table_name}".format(
+                        "duplicate table: '{table_name}'".format(
                             table_name=table["table_name"]
                         )
                     )
@@ -40,9 +40,27 @@ class Table(AppZero):
                 for field in table["table_fields"]:
                     if field["field_name"] in existed_field:
                         raise Exception(
-                            "duplicate field in {table_name}: {field_name}".format(
+                            "duplicate field in '{table_name}': '{field_name}'".format(
                                 table_name=table["table_name"],
                                 field_name=field["field_name"],
                             )
                         )
                     existed_field.append(field["field_name"])
+                    if field["field_type"] == "string" and "field_length" not in field:
+                        raise Exception(
+                            """
+                            unspecified 'field_length' for string typed field in '{table_name}': '{field_name}'
+                            """.format(
+                                table_name=table["table_name"],
+                                field_name=field["field_name"],
+                            )
+                        )
+                    if (
+                        field["field_type"] not in ["string"]
+                        and "field_length" in field
+                    ):
+                        raise Exception(
+                            "invalid property 'field_length' to type: '{field_type}'".format(
+                                field_type=field["field_type"]
+                            )
+                        )
