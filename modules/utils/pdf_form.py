14aa36272314a6d49d26e8bcf116a9017de7f485
chinapandaman <b.j.li@foxmail.com>
2019-12-21 11:07:58 -0600

    use disk file for now

--- modules/utils/pdf_form.py
+++ modules/utils/pdf_form.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
+import os
 import shutil
 import uuid
-from tempfile import NamedTemporaryFile
 
 import pdfrw
-from PyPDF2 import PdfFileReader
+
+from gluon import current
 
 
 class PDFForm(object):
@@ -18,19 +19,26 @@ class PDFForm(object):
     _WIDGET_SUBTYPE_KEY = "/Widget"
 
     def __init__(self):
-        self._template_file = NamedTemporaryFile(suffix=".pdf")
-        self._output_file = NamedTemporaryFile(suffix=".pdf")
-        self._final_file_with_image = NamedTemporaryFile(suffix=".pdf")
-        self._final_file = NamedTemporaryFile(suffix=".pdf")
-
         self._uuid = uuid.uuid4().hex
         self._data_dict = {}
 
-    def _fill_pdf(self):
-        template_pdf = pdfrw.PdfReader(self._template_file.name)
-        num_of_pages = PdfFileReader(self._template_file).getNumPages()
+        self._template_path = os.path.join(
+            current.request.folder, "temp", "template.pdf"
+        )
+        self._output_path = os.path.join(current.request.folder, "temp", "output.pdf")
+        self._final_file_with_image = os.path.join(
+            current.request.folder, "temp", "final_with_image.pdf"
+        )
+        self._final_file = os.path.join(
+            current.request.folder, "temp", self._uuid + ".pdf"
+        )
 
-        for i in range(num_of_pages):
+        self.pdf_stream = ""
+
+    def _fill_pdf(self):
+        template_pdf = pdfrw.PdfReader(self._template_path)
+
+        for i in range(len(template_pdf.pages)):
             annotations = template_pdf.pages[i][self._ANNOT_KEY]
             if annotations:
                 for annotation in annotations:
@@ -47,7 +55,9 @@ class PDFForm(object):
                                 )
                             )
 
-        pdfrw.PdfWriter().write(self._output_file.name, template_pdf)
+        pdfrw.PdfWriter().write(self._output_path, template_pdf)
+        with open(self._output_path, "rb+") as f:
+            self.pdf_stream = f.read()
 
     def bools_to_checkboxes(self):
         for k, v in self._data_dict.items():
@@ -59,7 +69,8 @@ class PDFForm(object):
 
     def build_pdf(self, template_file, data_dict, canvas=False, global_font_size=8):
         if template_file:
-            shutil.copyfileobj(template_file, self._template_file)
+            with open(self._template_path, "wb+") as f:
+                shutil.copyfileobj(template_file, f)
 
         self._data_dict = data_dict
         self.bools_to_checkboxes()
