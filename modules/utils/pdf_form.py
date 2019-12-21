2ff845a06d8fcc1f7e4bdbb554b1c10757ab3132
chinapandaman <b.j.li@foxmail.com>
2019-12-21 11:19:13 -0600

    remove temp file

--- modules/utils/pdf_form.py
+++ modules/utils/pdf_form.py
@@ -13,7 +13,6 @@ from gluon import current
 class PDFForm(object):
     _ANNOT_KEY = "/Annots"
     _ANNOT_FIELD_KEY = "/T"
-    _ANNOT_VAL_KEY = "/V"
     _ANNOT_RECT_KEY = "/Rect"
     _SUBTYPE_KEY = "/Subtype"
     _WIDGET_SUBTYPE_KEY = "/Widget"
@@ -26,10 +25,10 @@ class PDFForm(object):
             current.request.folder, "temp", "template.pdf"
         )
         self._output_path = os.path.join(current.request.folder, "temp", "output.pdf")
-        self._final_file_with_image = os.path.join(
+        self._final_path_with_image = os.path.join(
             current.request.folder, "temp", "final_with_image.pdf"
         )
-        self._final_file = os.path.join(
+        self._final_path = os.path.join(
             current.request.folder, "temp", self._uuid + ".pdf"
         )
 
@@ -59,7 +58,7 @@ class PDFForm(object):
         with open(self._output_path, "rb+") as f:
             self.pdf_stream = f.read()
 
-    def bools_to_checkboxes(self):
+    def _bools_to_checkboxes(self):
         for k, v in self._data_dict.items():
             if isinstance(v, bool):
                 if not v:
@@ -67,14 +66,19 @@ class PDFForm(object):
                 else:
                     self._data_dict[k] = pdfrw.PdfName.Yes if v else pdfrw.PdfName.Off
 
+    def _remove_temp_file(self):
+        os.remove(self._template_path)
+        os.remove(self._output_path)
+
     def build_pdf(self, template_file, data_dict, canvas=False, global_font_size=8):
         if template_file:
             with open(self._template_path, "wb+") as f:
                 shutil.copyfileobj(template_file, f)
 
         self._data_dict = data_dict
-        self.bools_to_checkboxes()
+        self._bools_to_checkboxes()
 
         self._fill_pdf()
 
+        self._remove_temp_file()
         return self
