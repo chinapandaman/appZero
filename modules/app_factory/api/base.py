8c83f64707e67f9bff565d3b9313656972369806
chinapandaman <b.j.li@foxmail.com>
2019-12-24 21:07:32 -0600

    better message with http status code

--- modules/app_factory/api/base.py
+++ modules/app_factory/api/base.py
@@ -24,7 +24,7 @@ class API(AppZero):
             "{name}.json".format(name=param),
         )
         if not os.path.exists(self._definition_path):
-            raise HTTP(404)
+            raise HTTP(404, "cannot locate API definition '{name}'".format(name=param))
         self._schema_name = "api.schema.json"
         self._schema_path = os.path.join(
             current.request.folder, "static", "json_schema", "api_schema"
@@ -52,12 +52,11 @@ class API(AppZero):
                             "invalid queryable field '{field}'".format(field=each)
                         )
 
-            if "selectors" in self.data["GET"]:
-                for each in self.data["GET"]["selectors"]:
-                    if each not in self._db[self.data["table_name"]].fields:
-                        raise ValidationError(
-                            "invalid selector '{field}'".format(field=each)
-                        )
+            for each in self.data["GET"]["selectors"]:
+                if each not in self._db[self.data["table_name"]].fields:
+                    raise ValidationError(
+                        "invalid selector '{field}'".format(field=each)
+                    )
 
         if (
             "POST" in self.data["supported_methods"]
@@ -81,7 +80,10 @@ class API(AppZero):
 
     def _method_validate(self, method):
         if method not in self.data["supported_methods"]:
-            raise HTTP(405)
+            raise HTTP(
+                405,
+                "'{method}' method is not supported by this API".format(method=method),
+            )
 
     def get(self, table_id=None, additional_query=None):
         self._method_validate("GET")
@@ -113,7 +115,7 @@ class API(AppZero):
         if table_id:
             if result:
                 return result.first()
-            raise HTTP(404)
+            raise HTTP(404, "no record found")
 
         return result
 
@@ -123,12 +125,12 @@ class API(AppZero):
         if self.data["POST"].get("allowed_fields") and any(
             [each not in self.data["POST"]["allowed_fields"] for each in data.keys()]
         ):
-            raise HTTP(400)
+            raise HTTP(400, "unallowed POST field")
 
         if not self.data["POST"]["allow_duplicates"] and self._db[
             self.data["table_name"]
         ](**data):
-            raise HTTP(400)
+            raise HTTP(400, "duplicate record exists")
 
         return self._db[self.data["table_name"]].insert(**data)
 
@@ -138,7 +140,7 @@ class API(AppZero):
         if self.data["PUT"].get("allowed_fields") and any(
             [each not in self.data["PUT"]["allowed_fields"] for each in data.keys()]
         ):
-            raise HTTP(400)
+            raise HTTP(400, "unallowed PUT field")
 
         self._db(
             (self._db[self.data["table_name"]].is_active == True)
